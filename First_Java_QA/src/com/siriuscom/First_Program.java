265c115623223d52b217b094ef9bcb3e2bc48753
AwesMosian <avinashtavade@gmail.com>
2022-02-14 14:10:58 +0530

    ...
    First Program

---
+++ First_Java_QA/src/com/siriuscom/First_Program.java
@@ -0,0 +1,17 @@
+package com.siriuscom;
+
+public class First_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10, b = 15;
+		
+		int c = a * b;
+		
+		System.out.println("Product of given two no's is : "+ c );
+		
+
+	}
+
+}
